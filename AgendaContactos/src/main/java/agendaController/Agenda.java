d8086dd8fc17c80dad3b99477b648f7a5752fe04
jhovannysr <84175470+jhovannysr@users.noreply.github.com>
2023-10-02 09:57:14 +0200

    Update Agenda.java

--- AgendaContactos/src/main/java/agendaController/Agenda.java
+++ AgendaContactos/src/main/java/agendaController/Agenda.java
@@ -1,7 +1,15 @@
 package agendaController;
 
 public class Agenda {
+      
+      private String NOMBRE_FILE = "./agenda.csv";
+	RandomAccessFile file;
+      
+	public Agenda() throws FileNotFoundException {
+		file = new RandomAccessFile(NOMBRE_FILE, "rw");
+	}
 
+      
       public void listarUsuarios() throws IOException {
         String archivoCSV = "./agendaCSV.csv";
         String linea;
