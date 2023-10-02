a7bb36dad8ed909964b02e6405ed7152a6c0a8eb
Pataplas <83818471+Pataplas@users.noreply.github.com>
2023-10-02 09:49:55 +0200

    Update Menu.java
    Cambiado listarUsuarios a agenda

--- AgendaContactos/src/main/java/menu/Menu.java
+++ AgendaContactos/src/main/java/menu/Menu.java
@@ -40,24 +40,6 @@ public class Menu {
 		return false;
     }
     
-    public void listarUsuarios() throws IOException {
-        String archivoCSV = "./agendaCSV.csv";
-        String linea;
-        
-        try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
-            while ((linea = br.readLine()) != null) {
-                // Dividir la línea en campos utilizando una coma como delimitador
-                String[] campos = linea.split(",");
-                
-                // Mostrar o almacenar los campos según sea necesario
-                for (String campo : campos) {
-                    System.out.print(campo + " ");
-                }
-                System.out.println(); // Salto de línea para la siguiente fila
-            }
-        }
-    }
-
     while (raf.getFilePointer()<raf.length())
         System.out.println(raf.readInt());
     	
