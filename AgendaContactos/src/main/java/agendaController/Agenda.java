942639be7dc72a24911ae02f076f3a573a184029
Pataplas <83818471+Pataplas@users.noreply.github.com>
2023-10-02 09:49:31 +0200

    Update Agenda.java

--- AgendaContactos/src/main/java/agendaController/Agenda.java
+++ AgendaContactos/src/main/java/agendaController/Agenda.java
@@ -2,4 +2,23 @@ package agendaController;
 
 public class Agenda {
 
+      public void listarUsuarios() throws IOException {
+        String archivoCSV = "./agendaCSV.csv";
+        String linea;
+        
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
+            while ((linea = br.readLine()) != null) {
+                // Dividir la línea en campos utilizando una coma como delimitador
+                String[] campos = linea.split(",");
+                
+                // Mostrar o almacenar los campos según sea necesario
+                for (String campo : campos) {
+                    System.out.print(campo + " ");
+                }
+                System.out.println(); // Salto de línea para la siguiente fila
+            }
+        }
+    }
+
+
 }
