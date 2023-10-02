9355266b28196f150249b3909c42ff0574e9841a
jhovannysr <84175470+jhovannysr@users.noreply.github.com>
2023-10-02 14:01:31 +0200

    Update Agenda.java

--- AgendaContactos/src/main/java/agendaController/Agenda.java
+++ AgendaContactos/src/main/java/agendaController/Agenda.java
@@ -1,15 +1,53 @@
 package agendaController;
 
 public class Agenda {
-      
-      private String NOMBRE_FILE = "./agenda.csv";
-	RandomAccessFile file;
-      
+   
+	/**
+	 * Nombre del fichero
+	 */
+	private String NOMBRE_FILE = "./agenda.csv";
+	
+	/**
+	 * Tipo de fichero que vamos a utilizar
+	 */
+	private RandomAccessFile file;
+	
+	/**
+	 * Constructor en el que inicializamos RandomAccessFile
+	 * @throws FileNotFoundException
+	 */
 	public Agenda() throws FileNotFoundException {
 		file = new RandomAccessFile(NOMBRE_FILE, "rw");
 	}
 
-      
+	/** (Jhovanny)
+	 * Posiciona el puntero en la última línea para ingresar nuevos registros 
+	 * después del último registro que contiene el fichero
+	 * @param contacto
+	 * @return
+	 * @throws IOException
+	 */
+	public boolean crearContacto(Contacto contacto) throws IOException {
+		long posicion = file.length();
+		file.seek(posicion);
+		almacenarContacto(contacto);
+		return true;
+	}
+	
+	/** (Jhovanny)
+	 * Almacena el registro al fichero
+	 * @param contacto
+	 * @throws IOException
+	 */
+	public void almacenarContacto(Contacto contacto) throws IOException {
+		String registro = String.format("%s,%s,%s,%d\n", 
+				contacto.getUuid().toString(),
+				contacto.getNombre(),
+				contacto.getTelefono(),
+				contacto.getEdad());
+		file.writeBytes(registro);
+	}
+	
       public void listarUsuarios() throws IOException {
         String archivoCSV = "./agendaCSV.csv";
         String linea;
