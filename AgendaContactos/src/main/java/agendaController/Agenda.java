92049cd58bfe73dda3b682f2e552d9cfda1ff58d
Jean Paul <124582378+Gobliin@users.noreply.github.com>
2023-10-02 21:08:52 +0200

    Update Agenda.java

--- AgendaContactos/src/main/java/agendaController/Agenda.java
+++ AgendaContactos/src/main/java/agendaController/Agenda.java
@@ -16,10 +16,38 @@ public class Agenda {
 	 * Constructor en el que inicializamos RandomAccessFile
 	 * @throws FileNotFoundException
 	 */
+	
 	public Agenda() throws FileNotFoundException {
 		file = new RandomAccessFile(NOMBRE_FILE, "rw");
 	}
 
+	/*
+	 * (JeanPaul)
+	 */
+	public Contacto read(String nombre, UUID uuid) throws IOException {
+		long posicion = 0L;
+		file.seek(posicion);
+		while (file.getFilePointer() < file.length()) {
+			Contacto c = read();
+			if (c.getNombre().equals(nombre)){				
+			}else {
+				c.getUuid().equals(uuid);
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	private Contacto read() throws IOException {
+		String registro = file.readLine();
+		String[] campos = registro.split(",");
+		Contacto c = new Contacto(
+				campos[0],
+				campos[1],
+				Integer.parseInt(campos[2])
+			);
+		return c;
+	}
 	/** (Jhovanny)
 	 * Posiciona el puntero en la última línea para ingresar nuevos registros 
 	 * después del último registro que contiene el fichero
