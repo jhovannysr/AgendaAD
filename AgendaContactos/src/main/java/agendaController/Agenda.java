f8f9b47e6dc38980bd3828f97413f3ddb52cd0e1
jhovannysr <84175470+jhovannysr@users.noreply.github.com>
2023-10-03 10:11:31 +0200

    Update Agenda.java

--- AgendaContactos/src/main/java/agendaController/Agenda.java
+++ AgendaContactos/src/main/java/agendaController/Agenda.java
@@ -1,7 +1,14 @@
 package agendaController;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.UUID;
+
+import classes.Contacto;
+
 public class Agenda {
-   
+
 	/**
 	 * Nombre del fichero
 	 */
@@ -21,33 +28,6 @@ public class Agenda {
 		file = new RandomAccessFile(NOMBRE_FILE, "rw");
 	}
 
-	/*
-	 * (JeanPaul)
-	 */
-	public Contacto read(String nombre, UUID uuid) throws IOException {
-		long posicion = 0L;
-		file.seek(posicion);
-		while (file.getFilePointer() < file.length()) {
-			Contacto c = read();
-			if (c.getNombre().equals(nombre)){				
-			}else {
-				c.getUuid().equals(uuid);
-				return c;
-			}
-		}
-		return null;
-	}
-	
-	private Contacto read() throws IOException {
-		String registro = file.readLine();
-		String[] campos = registro.split(",");
-		Contacto c = new Contacto(
-				campos[0],
-				campos[1],
-				Integer.parseInt(campos[2])
-			);
-		return c;
-	}
 	/** (Jhovanny)
 	 * Posiciona el puntero en la última línea para ingresar nuevos registros 
 	 * después del último registro que contiene el fichero
@@ -76,26 +56,72 @@ public class Agenda {
 		file.writeBytes(registro);
 	}
 	
-public void listarUsuarios() throws IOException {
-    String archivoCSV = "./agendaCSV.csv";
+	/** (Cristian)
+	 * @throws IOException
+	 */
+	public void lista() throws IOException {
+	    try  {
+	        String linea;
+	        file.seek(0L);
+	        while ((linea = file.readLine()) != null) {
+	            // Dividir la línea en campos utilizando una coma como delimitador
+	            String[] campos = linea.split(",");
+	            
+	            // Mostrar o almacenar los campos separados por comas
+	            for (int i = 0; i < campos.length; i++) {
+	                System.out.print(campos[i] + ",");
+	            }
+	            System.out.println(); // Salto de línea para la siguiente fila
+	        }
+	    } catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * (JeanPaul)
+	 * @param nombre
+	 * @return
+	 * @throws IOException
+	 */public Contacto consultaPorNombre(String nombre) throws IOException {
+		long posicion = 0L;
+		file.seek(posicion);
+		while (file.getFilePointer() < file.length()) {
+			Contacto c = leerContacto();
+			if (c.getNombre().equals(nombre)) {
+				return c;
+			} 
+		}
+		return null;
+	}
+	
+	/**
+	 * (JeanPaul)
+	 * @param codigo
+	 * @return
+	 * @throws IOException
+	 */
+	public Contacto consultaPorCodigo(String codigo) throws IOException {
+		long posicion = 0L;
+		file.seek(posicion);
+		while (file.getFilePointer() < file.length()) {
+			Contacto c = leerContacto();
+			if (c.getUuid().toString().equals(codigo)) {
+				return c;
+			} 
+		}
+		return null;
+	}
+	
 
-    try (RandomAccessFile raf = new RandomAccessFile(archivoCSV, "rw")) {
-        String linea;
-        while ((linea = raf.readLine()) != null) {
-            // Dividir la línea en campos utilizando una coma como delimitador
-            String[] campos = linea.split(",");
-            
-            // Mostrar o almacenar los campos separados por comas
-            for (int i = 0; i < campos.length; i++) {
-                System.out.print(campos[i]);
-                if (i < campos.length - 1) {
-                    System.out.print(", ");
-                }
-            }
-            System.out.println(); // Salto de línea para la siguiente fila
-        }
-    }
-}
-
-
+	private Contacto leerContacto() throws IOException {
+		String registro = file.readLine();
+		String[] campos = registro.split(",");
+		Contacto c = new Contacto(
+				UUID.fromString(campos[0]), 
+				campos[1],
+				campos[2],
+				Integer.parseInt(campos[3]));
+		return c;
+	}
 }
