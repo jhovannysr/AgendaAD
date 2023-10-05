48920408c23685bfbd49799a8f6b041346d4fbd4
jhovannysr <84175470+jhovannysr@users.noreply.github.com>
2023-10-05 16:38:55 +0200

    Add files via upload

---
+++ AgendaContactos/src/main/java/prueba/MainPrueba.java
@@ -0,0 +1,18 @@
+package prueba;
+
+public class MainPrueba {
+	public static void main(String[] args) {
+		String cadena = "PeruAmadeo";
+		
+		String [] array = new String[cadena.length()];
+		
+		boolean coincidir = false;
+		boolean noCoincidir = false;
+		
+//		for (int i = 0; i < array.length; i++) {
+//			for (int j = 0; j < array.length; j++) {
+//				cadena.charAt(i) 
+//			}
+//		}
+	}
+}
